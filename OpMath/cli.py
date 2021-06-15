fcc29564ead2ee6792c81c0dfba12349685d3c31
Djbernas10 <bernardo.gomes2011@outlook.com>
2021-06-15 15:51:42 +0100

    Second Commit

---
+++ OpMath/cli.py
@@ -0,0 +1,19 @@
+from OpMath import *
+
+import click
+
+@click.command()
+@click.option("-m", '--func', help='function', required=True)
+@click.option("-n1", '--num1', help='num1', required=True)
+@click.option("-n2", '--num2', help='num2', required=True)
+
+def main(func, num1, num2):
+    if func=="soma":
+        num1 + num2
+    elif func=="menor":
+        num1 - num2
+    else:
+        print("Introduza 'soma', ou 'sub'.")
+
+if __name__ == '__main__':
+    main()
--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
+import os
+
+requirementPath = 'requirements.txt'
+install_requires =[]
+
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
 from setuptools import setup, find_packages
 
 setup(
     name="OpMath",
-    version="0.1",
-    description="OpMath - Funcoes de matemática",
+    version="0.2",
+    description="OpMath Distribution Package",
     packages=find_packages(),
-    include_package_data=True
-)
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'OpMath=OpMath.cli:main'
+            ]
+        }
+    )
